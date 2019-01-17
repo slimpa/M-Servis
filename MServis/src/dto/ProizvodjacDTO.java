f125773dfb3fb051594cc3880bd3be3f928fe6f6
ivanamalesevic <ivanamalesevic147@gmail.com>
2019-01-17 21:33:00 +0100

    uradjen dto proizvodjac

--- MServis/src/dto/ProizvodjacDTO.java
+++ MServis/src/dto/ProizvodjacDTO.java
@@ -2,7 +2,30 @@ package dto;
 
 public class ProizvodjacDTO {
 
-	private int idProizvodjac;
-	private String Naziv;
+    private int idProizvodjac;  
+    private String naziv;
 
+    public ProizvodjacDTO(int idProizvodjac, String naziv) {
+        this.idProizvodjac = idProizvodjac;
+        this.naziv = naziv;
+    }
+
+    public int getIdProizvodjac() {
+        return idProizvodjac;
+    }
+
+    public void setIdProizvodjac(int idProizvodjac) {
+        this.idProizvodjac = idProizvodjac;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+        
+        
+        
 }
