4a98833d03be6a31fb8e3f7fea99241b15a431fe
Bojan Gajic <bokad@live.com>
2019-01-25 15:21:42 +0100

    opet popravke mog zajeba

--- MServis/src/mySQL/MySQLProizvodjacDAO.java
+++ MServis/src/mySQL/MySQLProizvodjacDAO.java
@@ -20,7 +20,7 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 	 * @param proizvodjac
 	 */
     
-        public static final String SQL_INSERT = "insert into proizvodjac values (?, ?, ?)";
+        public static final String SQL_INSERT = "insert into proizvodjac (`Naziv`,`Obrisano`) values (?,?);";
 	public static final String SQL_SELECT = "select * from proizvodjac where Obrisano = 0";
 	public static final String SQL_UPDATE = "update proizvodjac set";
         
@@ -30,14 +30,23 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 		boolean returnValue = false;
 		
 		try {
+                    
+                    if(this.selectBy(proizvodjac).isEmpty() && this.selectIfRemoved(proizvodjac.getNaziv()).isEmpty()){
+                       
 			conn = ConnectionPool.getInstance().checkOut();
 			ps = conn.prepareStatement(SQL_INSERT);
-			ps.setInt(1, proizvodjac.getIdProizvodjac());
-			ps.setString(2, proizvodjac.getNaziv());
-                        ps.setInt(3, 0);
+			ps.setString(1, proizvodjac.getNaziv());
+                        ps.setInt(2, 0);
 			returnValue = ps.executeUpdate() == 1;
+                    } else if(!this.selectIfRemoved(proizvodjac.getNaziv()).isEmpty()){
+                        conn = ConnectionPool.getInstance().checkOut();
+			ps = conn.prepareStatement(SQL_UPDATE + " Obrisano = 0 where Naziv = ?");
+                        ps.setString(1, proizvodjac.getNaziv());
+			returnValue = ps.executeUpdate() == 1;
+                    }
 		} catch(SQLException e) {
 			e.printStackTrace();
+                        return false;
 		} finally {
 			ConnectionPool.getInstance().checkIn(conn);
 			DBUtil.getInstance().close(ps);			
@@ -65,7 +74,8 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 			
 			returnValue = ps.executeUpdate() == 1;
 		} catch(SQLException e) {
-			e.printStackTrace();
+                        return false;
+			//e.printStackTrace();
 		} finally {
 			ConnectionPool.getInstance().checkIn(conn);
 			DBUtil.getInstance().close(ps);
@@ -82,7 +92,7 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		boolean returnValue = false;
-		String query = SQL_UPDATE + " Obrisano = 1 where IdProizvodjac = ? and Obrisano = 0";
+		String query = SQL_UPDATE + " Obrisano = 1 where IdProizvodjac = ?";
 		
 		try {
 			conn = ConnectionPool.getInstance().checkOut();
@@ -114,6 +124,7 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 			
 			if(rs == null) return null;
 			else {
+                            
 				while(rs.next()) {
 					proizvodjaci.add(new ProizvodjacDTO(rs.getInt("IdProizvodjac"),rs.getString("Naziv")));
 				}
@@ -132,17 +143,17 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 	 * 
 	 * @param proizvodjac
 	 */
-	public List<ProizvodjacDTO> selectByName(String proizvodjac) {
+	public List<ProizvodjacDTO> selectBy(ProizvodjacDTO proizvodjac) {
 		List<ProizvodjacDTO> proizvodjaci = new ArrayList<ProizvodjacDTO>();
 		Connection conn = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		String query = SQL_SELECT + " and Naziv = ?";
+		String query = SQL_SELECT + " and Naziv like ?";
 		
 		try {
 			conn = ConnectionPool.getInstance().checkOut();
 			ps = conn.prepareStatement(query);
-			ps.setString(1, proizvodjac);
+			ps.setString(1, proizvodjac.getNaziv() + "%");
 			rs = ps.executeQuery();
 			
 			if(rs == null) return null;
@@ -161,9 +172,33 @@ public class MySQLProizvodjacDAO implements ProizvodjacDAO {
 		return proizvodjaci;
 	}
 
-    @Override
-    public List<ProizvodjacDTO> selectBy(ProizvodjacDTO proizvodjac) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   
+    public List<ProizvodjacDTO> selectIfRemoved(String naziv) {
+        List<ProizvodjacDTO> proizvodjaci = new ArrayList<ProizvodjacDTO>();
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String query = "select * from proizvodjac where Obrisano = 1 and Naziv = ?";
+		
+		try {
+			conn = ConnectionPool.getInstance().checkOut();
+			ps = conn.prepareStatement(query);
+			ps.setString(1, naziv);
+			rs = ps.executeQuery();
+			
+			if(rs == null) return null;
+			else {
+				while(rs.next()) {
+					proizvodjaci.add(new ProizvodjacDTO(rs.getInt("IdProizvodjac"), rs.getString("Naziv")));
+				}
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionPool.getInstance().checkIn(conn);
+			DBUtil.getInstance().close(ps);
+		}
+		
+		return proizvodjaci;
     }
-
 }
